ea10331333a9ad6405d848453b74e9ac7625dcb7
pozdnyakoks <pozdnyakoks@mail.ru>
2023-05-18 20:13:57 -0300

    refactor percentages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
 import { useEffect, useState } from 'react';
 
 function App() {
+  // апи работает с марта 2020 до февраля 2021 (включительно)
   const [hosp, setHosp] = useState([]);
-  const MAX_NUMBER = 10000;
+  const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let maxNumber = 0;
+
+  // you can change month here
+  let month = 4;
+  if (month < 10) month = `0${month}`;
 
   useEffect(() => {
-    const promises = [];
+    let promises = [];
     let repl;
-    for (let i = 1; i < 31; i++) {
+
+    for (let i = 1; i < months[month - 1] + 1; i++) {
       if (i < 10) repl = `0${i}`;
       promises.push(
         fetch(
-          `https://api.covidtracking.com/v2/us/daily/2020-11-${
+          `https://api.covidtracking.com/v2/us/daily/2020-${month}-${
             i < 10 ? repl : i
           }/simple.json`
-        ).then((res) => res.json())
+        )
+          .then((res) => {
+            if (res.ok) return res.json();
+            throw new Error();
+          })
+          .catch(() => (promises = []))
       );
     }
-    Promise.all(promises).then((data) => setHosp(data));
-  }, []);
+    Promise.all(promises)
+      .then((data) => setHosp(data))
+      .catch(() => {
+        return;
+      });
+  }, [month]);
+
+  maxNumber = tryMax() === 0 ? 10000 : tryMax();
+
+  function tryMax() {
+    try {
+      return Math.max(
+        ...hosp.map((info) => info.data.outcomes.hospitalized.currently)
+      );
+    } catch (er) {
+      return 1000;
+    }
+  }
 
   let cells = hosp.map((info, index) => {
-    const number = Math.round(info.data.outcomes.hospitalized.currently / 10);
-    const height = `${(number * 100) / MAX_NUMBER}`;
+    const number =
+      info.data === !info.data
+        ? 0
+        : info.data.outcomes.hospitalized.currently !== null
+        ? info.data.outcomes.hospitalized.currently
+        : 0;
+    let height = 0;
+    let percentage = (number * 100) / maxNumber;
+    if (percentage <= 10) {
+      height = (percentage * 40) / 10;
+    } else if (percentage <= 20) {
+      height = ((percentage - 10) * 20) / 100 + 40;
+    } else if (percentage <= 50) {
+      height = ((((percentage - 20) * 100) / 30) * 20) / 100 + 60;
+    } else {
+      let perc = ((((percentage - 50) * 100) / 50) * 20) / 100;
+      height = perc + 80;
+    }
+    height = height === 0 ? 3 : height;
+
     return (
       <div
         key={index}
         className='cell'
         style={{
-          height: `${height}%`,
+          height: `calc(${height}% - 7px)`,
         }}
       >
         <div className='popup'>{number}</div>
@@ -36,32 +82,43 @@ function App() {
     );
   });
 
+  function makeNumbersY() {
+    return (
+      <div className='left'>
+        <span>{maxNumber}</span>
+        <span>{Math.round((maxNumber * 50) / 100)}</span>
+        <span>{Math.round((maxNumber * 20) / 100)}</span>
+        <span>{Math.round((maxNumber * 10) / 100)}</span>
+        <span>{Math.round((maxNumber * 5) / 100)}</span>
+        <span>0</span>
+      </div>
+    );
+  }
+
+  function makeNumbersX() {
+    return (
+      <div className='dates'>
+        <span>01</span>
+        <span>05</span>
+        <span>10</span>
+        <span>15</span>
+        <span>20</span>
+        <span>25</span>
+        <span>{months[month - 1]}</span>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className='wrapper'>
         <h2 className='title'>Динамика дохода</h2>
-
         <div className='diagram'>
           <div className='top-block'>
-            <div className='left'>
-              <span>10000</span>
-              <span>5000</span>
-              <span>2000</span>
-              <span>1000</span>
-              <span>500</span>
-              <span>0</span>
-            </div>
-            <div className='graph'>{cells}</div>
+            {makeNumbersY()}
+            <div className='graph'>{cells.length > 0 && cells}</div>
           </div>
-          <div className='dates'>
-            <span>01</span>
-            <span>05</span>
-            <span>10</span>
-            <span>15</span>
-            <span>20</span>
-            <span>25</span>
-            <span>30</span>
-          </div>
+          {makeNumbersX()}
         </div>
       </div>
     </>
